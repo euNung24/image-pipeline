f45e31b2f958b43e6606ac110684239aa3215c48
EunYoung <pandamon24@naver.com>
2021-10-02 19:12:26 +0900

    changed url to checking screenshot

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://nungblog.com/board");
+  await page.goto("https://nungblog.com/annual_plan");
   await page.screenshot({ path: "images/nungblog.png" });
 
   await browser.close();
